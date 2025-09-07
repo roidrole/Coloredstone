1dbdb110bf41c58655fa24b9bab37478e667ee0e
roidrole <roidorle@gmail.com>
2025-09-06 20:39:02 -0400

    Partial first implementation
    To fix :
    - Getting the item textures (models already done, I use the vanilla redstone as placeholders)
    - Figure out how to make sideways-connected comparators and repeaters connect
    - Consider adding coloured redstone puffs

---
+++ src/main/java/roidrole/coloredstone/BlockColorstoneWire.java
@@ -0,0 +1,76 @@
+package roidrole.coloredstone;
+
+import net.minecraft.block.BlockRedstoneWire;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.awt.*;
+import java.util.Random;
+
+public class BlockColorstoneWire extends BlockRedstoneWire {
+	public final EnumDyeColor color;
+	public final Color defaultColor;
+
+	public BlockColorstoneWire(EnumDyeColor color){
+		super();
+		this.color = color;
+		this.defaultColor = new Color(color.getColorValue());
+		this.setRegistryName(Tags.MOD_ID, color.getName()+"stone_wire");
+		this.setTranslationKey(Tags.MOD_ID+"."+color.getName()+"stone_wire");
+		ColoredStone.blockArray[color.ordinal()] = this;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+		int i = state.getValue(POWER);
+		if(i == 0){return;}
+
+		double posX = (double)pos.getX() + 0.5D + (rand.nextDouble() - 0.5D) * 0.2D;
+		double posY = (double)pos.getY() + 0.0625D;
+		double posZ = (double)pos.getZ() + 0.5D + (rand.nextDouble() - 0.5D) * 0.2D;
+		double f = (double)i / 15.0D;
+		double speedX = f * 0.6D + 0.4D;
+		double speedY = Math.max(0.0D, f * f * 0.7D - 0.5D);
+		double speedZ = Math.max(0.0D, f * f * 0.6D - 0.7D);
+		world.spawnParticle(EnumParticleTypes.REDSTONE, posX, posY, posZ, speedX, speedY, speedZ);
+	}
+
+	@Override
+	public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
+		return new ItemStack(ColoredStone.itemMap.get(color));
+	}
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return ColoredStone.itemMap.get(color);
+	}
+
+	@SideOnly(Side.CLIENT)
+	public static int getColor(IBlockState state) {
+		Color defaultColor = ((BlockColorstoneWire)state.getBlock()).defaultColor;
+		float power = (float)(state.getValue(BlockRedstoneWire.POWER)+8)/16.0f;
+		return MathHelper.rgb(
+			MathHelper.clamp((int)(defaultColor.getRed() * power), 0, 255),
+			MathHelper.clamp((int)(defaultColor.getGreen() * power), 0, 255),
+			MathHelper.clamp((int)(defaultColor.getBlue() * power), 0, 255)
+		);
+	}
+
+	@Override
+	public int getMaxCurrentStrength(World world, BlockPos pos, int strength) {
+		IBlockState state = world.getBlockState(pos);
+		if(state.getBlock() instanceof BlockRedstoneWire){
+			return Math.max(state.getValue(POWER), strength);
+		}
+		return strength;
+	}
+}
---
+++ src/main/java/roidrole/coloredstone/ColoredStone.java
@@ -0,0 +1,63 @@
+package roidrole.coloredstone;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.EnumDyeColor;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLConstructionEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+public class ColoredStone {
+    static Map<EnumDyeColor, Item> itemMap = new EnumMap<>(EnumDyeColor.class);
+    static Block[] blockArray = new Block[16];
+
+    @Mod.EventHandler
+    public void construction(FMLConstructionEvent event){
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void registerBlockColours(ColorHandlerEvent.Block event){
+        event.getBlockColors().registerBlockColorHandler(
+			(state, worldIn, pos, tintIndex) ->
+                BlockColorstoneWire.getColor(state),
+            blockArray
+        );
+    }
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        for(EnumDyeColor color : EnumDyeColor.values()){
+            itemMap.put(color, new ItemBlock(new BlockColorstoneWire(color)){{
+                this.setRegistryName(block.getRegistryName());
+                this.setTranslationKey(block.getTranslationKey().substring(5));
+            }});
+        }
+        for(Item item : itemMap.values()){
+            ForgeRegistries.ITEMS.register(item);
+        }
+        for(Block block : blockArray){
+            ForgeRegistries.BLOCKS.register(block);
+        }
+    }
+
+    @SubscribeEvent
+    public void registerModels(ModelRegistryEvent event){
+        for(Item item : itemMap.values()){
+            ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(item.getRegistryName().toString()));
+        }
+    }
+}
